b011cb55bed165d4f0d989c68a72bfcf824ca27d
Dohyung0 <162238754+Dohyung0@users.noreply.github.com>
2024-04-19 13:33:44 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,10 @@
+for i in range(1,10):
+    print()
+    for j in range(2,6):
+        print(f'{j} x {i} = {j*i:2d}',end='    ')
+print()
+for i in range(1,10):
+    print()
+    for j in range(6,10):
+        print(f'{j} x {i} = {j*i:2d}',end='    ')
+
